5e9183f67bdc0080ac0b468a913f9582b8c5b866
miriam-estebanb <61233352+miriam-estebanb@users.noreply.github.com>
2021-03-26 13:51:05 +0100

    Add files via upload

---
+++ ATRAPA UN MILLON.c
@@ -0,0 +1,12 @@
+//ATRAPA UN MILLÓN
+#include<stdio.h>
+#include<string.h>//Funciones de las cadenas
+#define MAX_USUARIOS 100
+
+struct usuario{
+
+	char nombreUsuario[100];
+	char contrasena[200];
+};
+
+
