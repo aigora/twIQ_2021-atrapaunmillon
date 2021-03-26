587fc27f9c51e729a7df8d7967ec2c2a28ebb0e7
valeeria15 <80395097+valeeria15@users.noreply.github.com>
2021-03-26 14:14:33 +0100

    Update ATRAPA UN MILLON.c

--- ATRAPA UN MILLON.c
+++ ATRAPA UN MILLON.c
@@ -2,27 +2,6 @@
 #include<stdio.h>
 #include<string.h>//Funciones de las cadenas
 #define MAX_USUARIOS 100
-//opción1
-/*struct usuario{
-
-	char nombreUsuario[100];
-	char contrasena[200];
-};
-
-int main(){
-	int respuesta;
-printf("BIENVENIDO A ATRAPA UN MILLÓN)
-//A continuación aparecerá en pantalla un menú con 3 opciones que el usuario tendrá que elegir:
-printf("A.Registrarse\n");
-printf("B.Iniciar sesión\n");
-printf("C.Salir\n");
-       scanf("%d", respuesta);
-}*/
-//opción2
-//ATRAPA UN MILLÓN
-/*#include<stdio.h>
-#include<string.h>//Funciones de las cadenas
-#define MAX_USUARIOS 100
 
 struct usuario{
 
@@ -43,5 +22,13 @@ int main(){
 		printf("|d-Salir                      |\n");
 		printf("|_____________________________|\n");
 	}
+	
+switch(respuesta){
+	case A:
+	  printf("A.Registrarse\n");
+	  printf("Introduzca su nombre:\n);
+	       scanf("%s", nombreUsuario);
+	  printf("%s ya puede empezar a jugar.¡Suerte!", nombreUsuario);
+		 break;
 }*/
 
