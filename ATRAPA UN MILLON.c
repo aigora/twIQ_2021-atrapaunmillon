abe3d0b7a0b00be8ad0aeb02c3a8270f4261f906
miriam-estebanb <61233352+miriam-estebanb@users.noreply.github.com>
2021-03-26 13:58:02 +0100

    Update ATRAPA UN MILLON.c

--- ATRAPA UN MILLON.c
+++ ATRAPA UN MILLON.c
@@ -1,4 +1,4 @@
-//ATRAPA UN MILLÓN
+//ATRAPA UN MILLÃ“N
 #include<stdio.h>
 #include<string.h>//Funciones de las cadenas
 #define MAX_USUARIOS 100
@@ -9,4 +9,13 @@ struct usuario{
 	char contrasena[200];
 };
 
-
+do{	
+	system("cls");//Borrar pantalla	
+	printf("Atrapa Un Millon\n Bienvenido al menu opciones:\n ");
+	printf("Introduce la opcion:\n");
+	printf("-----------------------\n");
+	printf("a-Iniciar sesion para jugar\n");
+	printf("b-Registrarse\n");
+	printf("c-Intrucciones\n");
+	printf("d-Salir\n");
+	printf("-----------------------\n");
