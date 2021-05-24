03a0c87b295bd2421b37b44ae169fbec9cded787
anagarcia2706 <80408753+anagarcia2706@users.noreply.github.com>
2021-05-25 00:01:32 +0200

    Update ATRAPA UN MILLON.c

--- ATRAPA UN MILLON.c
+++ ATRAPA UN MILLON.c
@@ -14,9 +14,26 @@ int main(){
 	char password;
 	int copia=0;
 	int numUsuario;
-	int apuesta1A,apuesta1B,apuesta1C;
 	struct usuario u[i];
 	FILE * aumfichero;
+	
+	int dinero=1000000;
+	struct apuestas apuestaPregunta1;
+	struct preguntas pregunta1={"¿Que es un crotalo", "Un lagarto", "Un pajaro", "Una serpiente"};
+	struct apuestas apuestaPregunta2;
+	struct preguntas pregunta2={"¿Quien escribio El Principito?", "Antoine de Saint-Exupery", "Jorge-Luis Borges", "Victor Hugo"};
+	struct apuestas apuestaPregunta3;
+	struct preguntas pregunta3={"¿Cual era la capital del Imperio Inca?", "Lima", "Cuzco", "Macchu Picchu"};
+	struct apuestas apuestaPregunta4;
+	struct preguntas pregunta4={"¿Quien es el autor de La divina comedia?", "Dante", "Maquiavelo", "Petrarca"};
+	struct apuestas apuestaPregunta5;
+	struct preguntas pregunta5={"La columna mas a la derecha de la tabla periodica esta compuesta por:", "Haluros", "Alcalinos", "Gases nobles"};
+	struct apuestas apuestaPregunta6;
+	struct preguntas pregunta6={"¿Cual es la capital de Canada?", "Toronto", "Ottawa", "Vancouver"};
+	struct apuestas apuestaPregunta7;
+	struct preguntas pregunta7={"¿Quien era el dios romano de la guerra?", "Ares", "Marte", "Belerofonte"};
+	struct apuestas apuestaPregunta8;
+	struct preguntas pregunta8={"¿En que comunidad autonoma nace el rio Ebro?", "Cantabria", "Aragon", "La Rioja"};
 	
 	do{
 		system("cls");//Borrar pantalla	
@@ -195,63 +212,231 @@ int main(){
 					break;
 		}
 	
-	if(opcion=='a' || opcion=='b'){
+	if(respuesta=='a' || respuesta=='b'){
 		printf("Ya puede empezar a jugar¡Suerte!\n");
-		do{	
-			printf("\n**PREGUNTA 1**\n");
-			printf("¿Qué es un crótalo?\n");
-			printf("OPCION A: Un lagarto\n");
-			printf("OPCION B: Un pájaro\n");
-			printf("OPCION C: Una serpiente\n");
-				
+
+		// Hacemos la primera pregunta
+		imprimirPregunta (1,pregunta1.pregunta, pregunta1.respuestaA, pregunta1.respuestaB, pregunta1.respuestaC);
+
+		printf("Tienes %d euros\n", dinero);
+		do {
+			printf("¿Cuanto apuestas por la opcion A? (Tiene que ser divisible por 1000)\n");
+			do {
+				scanf("%d", &apuestaPregunta1.apuestaA);
+			} while (apuestaPregunta1.apuestaA % 1000 != 0);
+
+			printf("¿Cuanto apuestas por la opcion B? (Tiene que ser divisible por 1000)\n");
+			do {
+				scanf("%d", &apuestaPregunta1.apuestaB);
+			} while (apuestaPregunta1.apuestaB % 1000 != 0);
+
+			printf("¿Cuanto apuestas por la opcion C? (Tiene que ser divisible por 1000)\n");
+			do {
+				scanf("%d", &apuestaPregunta1.apuestaC);
+			} while (apuestaPregunta1.apuestaC % 1000 != 0);
+		} while (apuestaPregunta1.apuestaA + apuestaPregunta1.apuestaB + apuestaPregunta1.apuestaC != dinero);
+		dinero= apuestaPregunta1.apuestaC;
+		if (dinero!=0)  {
+			printf("La opcion correcta es la C. Te quedan %d euros\n", dinero);
+
+			//segunda pregunta
+			imprimirPregunta (2,pregunta2.pregunta, pregunta2.respuestaA, pregunta2.respuestaB, pregunta2.respuestaC);
+
 			printf("Tienes %d euros\n", dinero);
 			do {
 				printf("¿Cuanto apuestas por la opcion A? (Tiene que ser divisible por 1000)\n");
 				do {
-					scanf("%d", &apuesta1A);
-				} while (apuesta1A % 1000 != 0);
-					
+					scanf("%d", &apuestaPregunta2.apuestaA);
+				} while (apuestaPregunta2.apuestaA % 1000 != 0);
+
 				printf("¿Cuanto apuestas por la opcion B? (Tiene que ser divisible por 1000)\n");
 				do {
-					scanf("%d", &apuesta1B);
-				} while (apuesta1B % 1000 != 0);
-					
+					scanf("%d", &apuestaPregunta2.apuestaB);
+				} while (apuestaPregunta2.apuestaB % 1000 != 0);
+
 				printf("¿Cuanto apuestas por la opcion C? (Tiene que ser divisible por 1000)\n");
 				do {
-					scanf("%d", &apuesta1C);
-				} while (apuesta1C % 1000 != 0);
-			} while (apuesta1A + apuesta1B + apuesta1C != dinero);
-			dinero= apuesta1C;
-			printf("La opcion correcta es la C. Te quedan %d euros\n", dinero); 
-		} while 
-		if (dinero != 0) {
-			do{	
-				printf("\n**PREGUNTA 2**\n");
-				printf("¿Quién escribió El Principito?\n");
-				printf("OPCION A: Antoine de Saint-Exupery\n");
-				printf("OPCION B: Jorge-Luis Borges\n");
-				printf("OPCION C: Victor Hugo\n");
-				
+					scanf("%d", &apuestaPregunta2.apuestaC);
+				} while (apuestaPregunta2.apuestaC % 1000 != 0);
+			} while (apuestaPregunta2.apuestaA + apuestaPregunta2.apuestaB + apuestaPregunta2.apuestaC != dinero);
+			dinero= apuestaPregunta2.apuestaA;
+			if (dinero!=0)  {
+				printf("La opcion correcta es la A. Te quedan %d euros\n", dinero);
+
+				//tercera pregunta
+				imprimirPregunta (3,pregunta3.pregunta, pregunta3.respuestaA, pregunta3.respuestaB, pregunta3.respuestaC);
+
 				printf("Tienes %d euros\n", dinero);
 				do {
 					printf("¿Cuanto apuestas por la opcion A? (Tiene que ser divisible por 1000)\n");
 					do {
-						scanf("%d", &apuesta2A);
-					} while (apuesta1A % 1000 != 0);
+						scanf("%d", &apuestaPregunta3.apuestaA);
+					} while (apuestaPregunta3.apuestaA % 1000 != 0);
+
 					printf("¿Cuanto apuestas por la opcion B? (Tiene que ser divisible por 1000)\n");
 					do {
-						scanf("%d", &apuesta2B);
-					} while (apuesta1B % 1000 != 0);
-					
-				printf("¿Cuanto apuestas por la opcion C? (Tiene que ser divisible por 1000)\n");
-				do {
-					scanf("%d", &apuesta2C);
-				} while (apuesta1C % 1000 != 0);
-			} while (apuesta1A + apuesta1B + apuesta1C != dinero);
-			dinero= apuesta1C;
-			printf("La opcion correcta es la C. Te quedan %d euros\n", dinero);
+						scanf("%d", &apuestaPregunta3.apuestaB);
+					} while (apuestaPregunta3.apuestaB % 1000 != 0);
+
+					printf("¿Cuanto apuestas por la opcion C? (Tiene que ser divisible por 1000)\n");
+					do {
+						scanf("%d", &apuestaPregunta3.apuestaC);
+					} while (apuestaPregunta3.apuestaC % 1000 != 0);
+				} while (apuestaPregunta3.apuestaA + apuestaPregunta3.apuestaB + apuestaPregunta3.apuestaC != dinero);
+				dinero= apuestaPregunta3.apuestaB;
+				if (dinero!=0)  {
+					printf("La opcion correcta es la B. Te quedan %d euros\n", dinero);
+
+					//cuarta pregunta
+					imprimirPregunta (4,pregunta4.pregunta, pregunta4.respuestaA, pregunta4.respuestaB, pregunta4.respuestaC);
+
+					printf("Tienes %d euros\n", dinero);
+					do {
+						printf("¿Cuanto apuestas por la opcion A? (Tiene que ser divisible por 1000)\n");
+						do {
+							scanf("%d", &apuestaPregunta4.apuestaA);
+						} while (apuestaPregunta4.apuestaA % 1000 != 0);
+
+						printf("¿Cuanto apuestas por la opcion B? (Tiene que ser divisible por 1000)\n");
+						do {
+							scanf("%d", &apuestaPregunta4.apuestaB);
+						} while (apuestaPregunta4.apuestaB % 1000 != 0);
+
+						printf("¿Cuanto apuestas por la opcion C? (Tiene que ser divisible por 1000)\n");
+						do {
+							scanf("%d", &apuestaPregunta4.apuestaC);
+						} while (apuestaPregunta4.apuestaC % 1000 != 0);
+					} while (apuestaPregunta4.apuestaA + apuestaPregunta4.apuestaB + apuestaPregunta4.apuestaC != dinero);
+					dinero= apuestaPregunta4.apuestaA;
+					if (dinero!=0)  {
+						printf("La opcion correcta es la A. Te quedan %d euros\n", dinero);
+
+						//quinta pregunta
+						imprimirPregunta (5,pregunta5.pregunta, pregunta5.respuestaA, pregunta5.respuestaB, pregunta5.respuestaC);
+
+						printf("Tienes %d euros\n", dinero);
+						do {
+							printf("¿Cuanto apuestas por la opcion A? (Tiene que ser divisible por 1000)\n");
+							do {
+								scanf("%d", &apuestaPregunta5.apuestaA);
+							} while (apuestaPregunta5.apuestaA % 1000 != 0);
+
+							printf("¿Cuanto apuestas por la opcion B? (Tiene que ser divisible por 1000)\n");
+							do {
+								scanf("%d", &apuestaPregunta5.apuestaB);
+							} while (apuestaPregunta5.apuestaB % 1000 != 0);
+
+							printf("¿Cuanto apuestas por la opcion C? (Tiene que ser divisible por 1000)\n");
+							do {
+								scanf("%d", &apuestaPregunta5.apuestaC);
+							} while (apuestaPregunta5.apuestaC % 1000 != 0);
+						} while (apuestaPregunta5.apuestaA + apuestaPregunta5.apuestaB + apuestaPregunta5.apuestaC != dinero);
+						dinero= apuestaPregunta5.apuestaC;
+						if (dinero!=0)  {
+							printf("La opcion correcta es la C. Te quedan %d euros\n", dinero);
+
+							//sexta pregunta
+							imprimirPregunta (6,pregunta6.pregunta, pregunta6.respuestaA, pregunta6.respuestaB, pregunta6.respuestaC);
+
+							printf("Tienes %d euros\n", dinero);
+							do {
+								printf("¿Cuanto apuestas por la opcion A? (Tiene que ser divisible por 1000)\n");
+								do {
+									scanf("%d", &apuestaPregunta6.apuestaA);
+								} while (apuestaPregunta6.apuestaA % 1000 != 0);
+
+								printf("¿Cuanto apuestas por la opcion B? (Tiene que ser divisible por 1000)\n");
+								do {
+									scanf("%d", &apuestaPregunta6.apuestaB);
+								} while (apuestaPregunta6.apuestaB % 1000 != 0);
+
+								printf("¿Cuanto apuestas por la opcion C? (Tiene que ser divisible por 1000)\n");
+								do {
+									scanf("%d", &apuestaPregunta6.apuestaC);
+								} while (apuestaPregunta6.apuestaC % 1000 != 0);
+							} while (apuestaPregunta6.apuestaA + apuestaPregunta6.apuestaB + apuestaPregunta6.apuestaC != dinero);
+							dinero= apuestaPregunta6.apuestaB;
+							if (dinero!=0)  {
+								printf("La opcion correcta es la B. Te quedan %d euros\n", dinero);
+
+								//septima pregunta
+								imprimirPregunta (7,pregunta7.pregunta, pregunta7.respuestaA, pregunta7.respuestaB, pregunta7.respuestaC);
+
+								printf("Tienes %d euros\n", dinero);
+								do {
+									printf("¿Cuanto apuestas por la opcion A? (Tiene que ser divisible por 1000)\n");
+									do {
+										scanf("%d", &apuestaPregunta7.apuestaA);
+									} while (apuestaPregunta7.apuestaA % 1000 != 0);
+
+									printf("¿Cuanto apuestas por la opcion B? (Tiene que ser divisible por 1000)\n");
+									do {
+										scanf("%d", &apuestaPregunta7.apuestaB);
+									} while (apuestaPregunta7.apuestaB % 1000 != 0);
+
+									printf("¿Cuanto apuestas por la opcion C? (Tiene que ser divisible por 1000)\n");
+									do {
+										scanf("%d", &apuestaPregunta7.apuestaC);
+									} while (apuestaPregunta7.apuestaC % 1000 != 0);
+								} while (apuestaPregunta7.apuestaA + apuestaPregunta7.apuestaB + apuestaPregunta7.apuestaC != dinero);
+								dinero= apuestaPregunta7.apuestaB;
+								if (dinero!=0)  {
+									printf("La opcion correcta es la B. Te quedan %d euros\n", dinero);
+
+									//octava pregunta
+									imprimirPregunta (8,pregunta8.pregunta, pregunta8.respuestaA, pregunta8.respuestaB, pregunta8.respuestaC);
+
+									printf("Tienes %d euros\n", dinero);
+									do {
+										printf("¿Cuanto apuestas por la opcion A? (Tiene que ser divisible por 1000)\n");
+										do {
+											scanf("%d", &apuestaPregunta8.apuestaA);
+										} while (apuestaPregunta8.apuestaA % 1000 != 0);
+
+										printf("¿Cuanto apuestas por la opcion B? (Tiene que ser divisible por 1000)\n");
+										do {
+											scanf("%d", &apuestaPregunta8.apuestaB);
+										} while (apuestaPregunta8.apuestaB % 1000 != 0);
+
+										printf("¿Cuanto apuestas por la opcion C? (Tiene que ser divisible por 1000)\n");
+										do {
+											scanf("%d", &apuestaPregunta8.apuestaC);
+										} while (apuestaPregunta8.apuestaC % 1000 != 0);
+									} while (apuestaPregunta8.apuestaA + apuestaPregunta8.apuestaB + apuestaPregunta8.apuestaC != dinero);
+									dinero= apuestaPregunta8.apuestaA;
+									if (dinero!=0)  {
+										printf("La opcion correcta es la A. Enhorabuena!! Ha conseguido ganar %d euros.", dinero);
+									} else {
+										printf("Lo sentimos pero se ha quedado sin dinero. Intentelo otra vez. Gracias.\n");
+										return 0;
+									}
+								} else {
+									printf("Lo sentimos pero se ha quedado sin dinero. Intentelo otra vez. Gracias.\n");
+									return 0;
+								}
+							} else {
+								printf("Lo sentimos pero se ha quedado sin dinero. Intentelo otra vez. Gracias.\n");
+								return 0;
+							}
+						} else {
+							printf("Lo sentimos pero se ha quedado sin dinero. Intentelo otra vez. Gracias.\n");
+							return 0;
+						}
+					} else {
+						printf("Lo sentimos pero se ha quedado sin dinero. Intentelo otra vez. Gracias.\n");
+						return 0;
+					}
+				} else {
+					printf("Lo sentimos pero se ha quedado sin dinero. Intentelo otra vez. Gracias.\n");
+					return 0;
+				}
+			} else {
+				printf("Lo sentimos pero se ha quedado sin dinero. Intentelo otra vez. Gracias.\n");
+				return 0;
+			}
 		} else {
-				printf("Te has quedado sin dinero. Intentalo de nuevo.\n")
+			printf("Lo sentimos pero se ha quedado sin dinero. Intentelo otra vez. Gracias.\n");
+			return 0;
 		}
 	}
 	}while(respuesta!='d');
